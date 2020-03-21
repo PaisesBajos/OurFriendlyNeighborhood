972c91c6730cc4970d751fd901a3d6c0e66e471d
ga25woc <michael.holland@tum.de>
2020-03-21 22:36:16 +0100

    distance between addresses in km

--- OurAwesomeBlog/src/main/java/de/awacademy/ourblog/utils/GetGPSfromApi.java
+++ OurAwesomeBlog/src/main/java/de/awacademy/ourblog/utils/GetGPSfromApi.java
@@ -12,7 +12,7 @@ import java.util.SortedMap;
 
 
 public class GetGPSfromApi {
-    public Map<String, Double> getLocationFromCityStreetNumber(String city, String street, int number) throws IOException {
+    public static HashMap<String, Double> getLocationFromCityStreetNumber(String city, String street, int number) throws IOException {
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> response = restTemplate.getForEntity("http://open.mapquestapi.com/geocoding/v1/address?key="+apiKey.getApiKey()+"&city="+city+"&street="+street+"+"+number+"&maxResults=1", String.class);
         ObjectMapper mapper = new ObjectMapper();
@@ -21,7 +21,25 @@ public class GetGPSfromApi {
             put("lat", root.findPath("lat").asDouble());
             put("lng", root.findPath("lng").asDouble());
         }};
-
     }
-
+    public static double distanceBetweenAddresses(Address addressOne, Address addressTwo){
+        HashMap<String, Double> posOne = new HashMap<>();
+        HashMap<String, Double> posTwo = new HashMap<>();
+        try{
+            posOne = getLocationFromCityStreetNumber(addressOne.getCityName(), addressOne.getStreetName(),addressOne.getNumber());
+            posTwo = getLocationFromCityStreetNumber(addressTwo.getCityName(), addressTwo.getStreetName(),addressTwo.getNumber());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        double lon1 = posOne.get("lng");
+        double lon2 = posTwo.get("lng");
+        double lat1 = posOne.get("lat");
+        double lat2 = posTwo.get("lat");
+        double theta = lon1 - lon2;
+        double dist = Math.sin(Math.toRadians(lat1)) * Math.sin(Math.toRadians(lat2)) + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) * Math.cos(Math.toRadians(theta));
+        dist = Math.acos(dist);
+        dist = Math.toDegrees(dist);
+        dist = dist * 111.1895;
+        return dist;
+    }
 }
