743bbf99d227bb1ba07de80084c77b9cb251c6b9
Maja Francetic <francetic.maja@gmail.com>
2020-03-22 11:54:07 +0100

    Fix layoutTasks html; fix a bug with login

--- OurAwesomeBlog/src/main/java/de/awacademy/ourblog/utils/AdressRepository.java
+++ OurAwesomeBlog/src/main/java/de/awacademy/ourblog/utils/AdressRepository.java
@@ -3,9 +3,12 @@ package de.awacademy.ourblog.utils;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AdressRepository extends CrudRepository<Adress, Long> {
+
+   List<Adress> findAllByOrderByPlzDesc();
 
 }
